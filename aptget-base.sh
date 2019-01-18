52e421c4f44c403a7f5adcfa076dfef95ee0b88d
Brian Haan <fishingfool.brian@gmail.com>
2019-01-17 17:27:50 -0700

    Updated some development/admin

--- aptget-base.sh
+++ aptget-base.sh
@@ -3,3 +3,11 @@ sudo apt-get install ubuntu-desktop
 sudo apt-get install build-essential chromium-browser docker.io
 sudo apt-get install git git-cola
 #Anaconda has to be downloaded
+
+#admin and network tools
+sudo apt-get install network-manager
+sudo apt-get install openvpn
+
+#development
+sudo apt-get openjdk-8-jdk openjdk-8-jre
+sudo apt-get vscode
